b89eda006600eab01790688d55182046a8554449
dario_ceni <dario01ceni@gmail.com>
2023-05-11 12:23:00 +0200

    second commit

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
-# This is a sample Python script.
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plost
+from PIL import Image
 
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Page setting
+st.set_page_config(layout="wide")
 
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Data
+seattle_weather = pd.read_csv('https://raw.githubusercontent.com/tvst/plost/master/data/seattle-weather.csv', parse_dates=['date'])
+stocks = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/stocks_toy.csv')
 
+# Row A
+a1, a2, a3 = st.columns(3)
+a1.image(Image.open('streamlit-logo-secondary-colormark-darktext.png'))
+a2.metric("Wind", "9 mph", "-8%")
+a3.metric("Humidity", "86%", "4%")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Row B
+b1, b2, b3, b4 = st.columns(4)
+b1.metric("Temperature", "70 °F", "1.2 °F")
+b2.metric("Wind", "9 mph", "-8%")
+b3.metric("Humidity", "86%", "4%")
+b4.metric("Humidity", "86%", "4%")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Row C
+c1, c2 = st.columns((7,3))
+with c1:
+    st.markdown('### Heatmap')
+    plost.time_hist(
+    data=seattle_weather,
+    date='date',
+    x_unit='week',
+    y_unit='day',
+    color='temp_max',
+    aggregate='median',
+    legend=None)
+with c2:
+    st.markdown('### Bar chart')
+    plost.donut_chart(
+        data=stocks,
+        theta='q2',
+        color='company')
