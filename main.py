07113156c1cf4739f07cc8dc8d40833f29a46d18
dario_ceni <dario01ceni@gmail.com>
2023-05-12 00:50:21 +0200

    plotly

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ for i, x in enumerate(data.created_at):
 
 data["Sommatoria"] = 1
 sorted(tweets_of_week)
-fig = px.bar(names=tweets_of_week.keys(), values=tweets_of_week.values())
+fig = px.bar(x=list(data.keys()), y=list(data.values()))
 # fig = px.bar(data, x='created_at', y='Sommatoria')
 fig.update_layout(xaxis_title="day", yaxis_title="number of tweets")
 st.plotly_chart(fig, use_container_width=False, sharing="streamlit", theme="streamlit")
