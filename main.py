f35e9d4777dfac9ef8391e2e9d7767a3bbbc5403
dario_ceni <dario01ceni@gmail.com>
2023-05-12 00:37:16 +0200

    plotly

--- main.py
+++ main.py
@@ -34,3 +34,12 @@ fig.update_layout(xaxis_title="day", yaxis_title="number of tweets")
 st.plotly_chart(fig, use_container_width=False, sharing="streamlit", theme="streamlit")
 
 
+# per creare pie chart xml_roberta
+score_for_xmlroberta_piechart = [0, 0, 0]
+labels = ["negative", "neutral", "positive"]
+for x in data.xlm_roberta_SCORE_numeric:
+    score_for_xmlroberta_piechart[int(x)+1] += 1
+
+print(score_for_xmlroberta_piechart)
+fig = px.pie(names=labels, values=score_for_xmlroberta_piechart)
+st.plotly_chart(fig, use_container_width=False, sharing="streamlit", theme="streamlit")
