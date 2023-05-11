66d6ec30ea627edf0926d2b0fca451ec268b27ea
dario_ceni <dario01ceni@gmail.com>
2023-05-12 00:15:33 +0200

    plotly

--- main.py
+++ main.py
@@ -29,36 +29,8 @@ for i, x in enumerate(data.created_at):
 
 data["Sommatoria"] = 1
 
-
-
-
 fig = px.bar(data, x='created_at', y='Sommatoria')
+fig.update_layout(xaxis_title = "day", yaxis_title = "number of tweets")
 st.plotly_chart(fig, use_container_width=False, sharing="streamlit", theme="streamlit")
 
 
-
-# Row B
-b1, b2, b3, b4 = st.columns(4)
-b1.metric("Temperature", "70 °F", "1.2 °F")
-b2.metric("Wind", "9 mph", "-8%")
-b3.metric("Humidity", "86%", "4%")
-b4.metric("Humidity", "86%", "4%")
-
-# # Row C
-# c1, c2 = st.columns((7,3))
-# with c1:
-#     st.markdown('### Heatmap')
-#     plost.time_hist(
-#     data=data,
-#     date='date',
-#     x_unit='week',
-#     y_unit='day',
-#     color='temp_max',
-#     aggregate='median',
-#     legend=None)
-# with c2:
-#     st.markdown('### Bar chart')
-#     plost.donut_chart(
-#         # data=stocks,
-#         theta='q2',
-#         color='company')
