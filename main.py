2ae05c2435f73093f894b52117184aa2293530b2
dario_ceni <dario01ceni@gmail.com>
2023-05-11 23:19:38 +0200

    icona

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import plost
 from PIL import Image
+from datetime import datetime
+import plotly.express as px
 
 # Page setting
 st.set_page_config(layout="wide", page_icon="Logo_of_Twitter.png",page_title="SenForWirn2023")
@@ -12,14 +14,25 @@ with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 # Data
-data = pd.read_csv('data/dataset_from_06_03_to_02_04.csv')
-stocks = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/stocks_toy.csv')
+data = pd.read_csv('data/data.csv', index_col=[0])
+
+data['created_at'] = pd.to_datetime(data['created_at'])
+start_date = datetime.strptime("2023-03-10 01:44:38+00:00", "%Y-%m-%d %H:%M:%S%z")
+end_date = datetime.strptime("2023-03-17 01:44:38+00:00", "%Y-%m-%d %H:%M:%S%z")
+mask = (data['created_at'] > start_date) & (data['created_at'] <= end_date)
+data = data.loc[mask]
+data.reset_index(drop=True, inplace=True)
+
 
 # Row A
-a1, a2, a3 = st.columns(3)
+a1, a2 = st.columns(3)
 a1.image(Image.open('Logo_of_Twitter.png'))
-a2.metric("Wind", data['text'].iloc[0], "-8%")
-a3.metric("Humidity", "86%", "4%")
+
+
+
+fig = px.bar(data["created_at"], x='year', y='pop')
+a2.st.plotly_chart(fig, use_container_width=False, sharing="streamlit", theme="streamlit")
+
 
 
 # Row B
@@ -29,21 +42,21 @@ b2.metric("Wind", "9 mph", "-8%")
 b3.metric("Humidity", "86%", "4%")
 b4.metric("Humidity", "86%", "4%")
 
-# Row C
-c1, c2 = st.columns((7,3))
-with c1:
-    st.markdown('### Heatmap')
-    plost.time_hist(
-    data=data,
-    date='date',
-    x_unit='week',
-    y_unit='day',
-    color='temp_max',
-    aggregate='median',
-    legend=None)
-with c2:
-    st.markdown('### Bar chart')
-    plost.donut_chart(
-        data=stocks,
-        theta='q2',
-        color='company')
+# # Row C
+# c1, c2 = st.columns((7,3))
+# with c1:
+#     st.markdown('### Heatmap')
+#     plost.time_hist(
+#     data=data,
+#     date='date',
+#     x_unit='week',
+#     y_unit='day',
+#     color='temp_max',
+#     aggregate='median',
+#     legend=None)
+# with c2:
+#     st.markdown('### Bar chart')
+#     plost.donut_chart(
+#         # data=stocks,
+#         theta='q2',
+#         color='company')
